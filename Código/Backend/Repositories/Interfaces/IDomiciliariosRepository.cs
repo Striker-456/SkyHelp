1d1e59b7f12d333322cb9d517de0494a37e692b6
khanon21 <sebastiantriana06@gmail.com>
2025-11-17 02:14:47 -0500

    ccambio arreglo del bool

--- Código/Backend/Repositories/Interfaces/IDomiciliariosRepository.cs
+++ Código/Backend/Repositories/Interfaces/IDomiciliariosRepository.cs
@@ -1,12 +1,16 @@
-﻿
-namespace SkyHelp.Repositories
+﻿using Microservicios;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SkyHelp.Repositories.Interfaces
 {
     public interface IDomiciliariosRepository
     {
-        Task<IEnumerable<Microservicios.Domiciliarios>> TodosLosDomiciliariosAsync();
-        Task<Microservicios.Domiciliarios> DomiciliariosIdAsync(Guid id);
-        Task<Microservicios.Domiciliarios> DomiciliariosCreadosAsync(Microservicios.Domiciliarios domiciliario);
-        Task<Microservicios.Domiciliarios> ActualizarDomiciliariosAsync(Microservicios.Domiciliarios domiciliario);
-        Task<bool> EliminarDomiciliariosAsync(Guid id);
+        Task<List<Domiciliarios>> ObtenerDomiciliarios();
+        Task<Domiciliarios> ObtenerDomiciliarioPorID(Guid id);
+        Task<bool> CrearDomiciliario(Domiciliarios domiciliario);
+        Task<bool> ActualizarDomiciliario(Domiciliarios domiciliario);
+        Task<bool> EliminarDomiciliario(Guid id);
     }
 }
