a4bfdb8348beb42eb4d9b76cb1e597f8acb38c89
Hector Velasquez <hectordv28@gmail.com>
2025-11-16 22:51:36 -0500

    Inclyendo auditoria en el context

--- Código/Backend/SkyHelpContext.cs
+++ Código/Backend/SkyHelpContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microservicios;
 using Microsoft.EntityFrameworkCore;
+using SkyHelp.Models;
 
 namespace SkyHelp.Context
 {
@@ -12,6 +13,7 @@ namespace SkyHelp.Context
         public DbSet<Usuarios> Usuarios { get; set; }
         public DbSet<Roles> Roles { get; set; }
         public DbSet<Articulos> Articulos { get; set; }
+        public DbSet<Auditoria> Auditoria { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -21,11 +23,15 @@ namespace SkyHelp.Context
             modelBuilder.Entity<Usuarios>(entity =>
             {
                 entity.HasKey(e => e.IDUsuarios);
+                entity.Property(e => e.IDRol).IsRequired();
                 entity.Property(e => e.NombreUsuarios).IsRequired().HasMaxLength(50);
                 entity.Property(e => e.NombreCompleto).IsRequired().HasMaxLength(100);
                 entity.Property(e => e.Correo).IsRequired().HasMaxLength(50);
-                entity.Property(e => e.Contraseña).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.Contrasena).IsRequired().HasMaxLength(50);
                 entity.Property(e => e.EstadoCuenta).IsRequired().HasMaxLength(50);
+                entity.HasOne(e => e.Rol)
+                      .WithMany(t => t.Usuario)
+                      .HasForeignKey(e => e.IDRol);
                 entity.ToTable("Usuarios");
             });
             // Configuración de la entidad Roles
@@ -47,6 +53,21 @@ namespace SkyHelp.Context
                 entity.Property(e => e.TotalVistas).IsRequired();
                 entity.Property(e => e.CalificacionPromedio).IsRequired();
                 entity.ToTable("Articulos");
+            });
+            // Configuración de la entidad Auditoria
+            modelBuilder.Entity<Auditoria>(entity =>
+            {
+                entity.HasKey(e => e.IDLog);
+                entity.Property(e => e.IDUsuario).IsRequired();
+                entity.Property(e => e.TipoEvento).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.TablaAfectada).IsRequired();
+                entity.Property(e => e.IDRegistro).IsRequired();
+                entity.Property(e => e.Descripcion).IsRequired().HasMaxLength(200);
+                entity.Property(e => e.FechaEvento).IsRequired();
+                entity.HasOne( e => e.Usuario)
+                      .WithMany(t => t.Auditorias)
+                      .HasForeignKey(e => e.IDUsuario);
+                entity.ToTable("Auditoria");
             });
 
             base.OnModelCreating(modelBuilder);
