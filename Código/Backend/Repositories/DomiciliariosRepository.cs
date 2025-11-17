2ae49dcef4de98182624642ed34ac46a16d24b78
khanon21 <sebastiantriana06@gmail.com>
2025-11-17 02:18:40 -0500

    arreglos en el domiciliarios repository

--- Código/Backend/Repositories/DomiciliariosRepository.cs
+++ Código/Backend/Repositories/DomiciliariosRepository.cs
@@ -1,52 +1,97 @@
-﻿using Microservicios;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using SkyHelp.Context;
-
+using SkyHelp.Repositories.Interfaces;
+using Microservicios;
 
 namespace SkyHelp.Repositories
 {
     public class DomiciliariosRepository : IDomiciliariosRepository
     {
-        private readonly SkyHelpContext _context;
-
+        private readonly SkyHelpContext _context; // Inyección de dependencia del contexto de la base de datos
+        
         public DomiciliariosRepository(SkyHelpContext context)
         {
             _context = context;
         }
 
-        public async Task<IEnumerable<Domiciliarios>> TodosLosDomiciliariosAsync()
+        public async Task<List<Domiciliarios>> ObtenerDomiciliarios()
         {
             return await _context.Domiciliarios.ToListAsync();
         }
 
-        public async Task<Domiciliarios> DomiciliariosIdAsync(Guid id)
+        public async Task<Domiciliarios> ObtenerDomiciliarioPorID(Guid id)
         {
-            return await _context.Domiciliarios.FindAsync(id);
-        }   
-        public async Task<Domiciliarios> DomiciliariosCreadosAsync(Domiciliarios domiciliario)
-        {
-            _context.Domiciliarios.Add(domiciliario);
-            await  _context.SaveChangesAsync();
-            return domiciliario;
-        }
-        
-        public async Task<Domiciliarios> ActualizarDomiciliariosAsync(Domiciliarios domiciliario)
-        {
-            _context.Domiciliarios.Update(domiciliario);
-            await _context.SaveChangesAsync();
-            return domiciliario;
-        }
-        public async Task<bool> EliminarDomiciliariosAsync(Guid id)
-        {
-            var domiciliario = await _context.Domiciliarios.FindAsync(id);
-            if (domiciliario == null)
-            {
-                return false;
-            }
-            _context.Domiciliarios.Remove(domiciliario);
-            await _context.SaveChangesAsync();
-            return true;
+            return await _context.Domiciliarios.FirstOrDefaultAsync(x => x.IDDomiciliario == id);
         }
 
+        public async Task<bool> CrearDomiciliario(Domiciliarios domiciliario)
+        {
+            try
+            {
+                await _context.Domiciliarios.AddAsync(domiciliario);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+                throw new Exception(ex.Message.ToString());
+            }
+        }
+
+        public async Task<bool> ActualizarDomiciliario(Domiciliarios domiciliario)
+        {
+            try
+            {
+                var domiciliarioExistente = await _context.Domiciliarios
+                    .FirstOrDefaultAsync(x => x.IDDomiciliario == domiciliario.IDDomiciliario);
+
+                if (domiciliarioExistente == null)
+                {
+                    return false;
+                    throw new Exception("Domiciliario para actualizar no existe");
+                }
+
+                domiciliarioExistente.NombreCompleto = domiciliario.NombreCompleto;
+                domiciliarioExistente.Telefono = domiciliario.Telefono;
+                domiciliarioExistente.Email = domiciliario.Email;
+                domiciliarioExistente.Estado = domiciliario.Estado;
+                domiciliarioExistente.PlacaVehiculo = domiciliario.PlacaVehiculo;
+                domiciliarioExistente.IDUsuario = domiciliario.IDUsuario;
+
+                _context.Domiciliarios.Update(domiciliarioExistente);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+                throw new Exception(ex.Message.ToString());
+            }
+        }
+
+        public async Task<bool> EliminarDomiciliario(Guid id)
+        {
+            try
+            {
+                var domiciliarioExistente = await _context.Domiciliarios
+                    .FirstOrDefaultAsync(x => x.IDDomiciliario == id);
+
+                if (domiciliarioExistente == null)
+                {
+                    return false;
+                    throw new Exception("Domiciliario para eliminar no existe");
+                }
+
+                _context.Domiciliarios.Remove(domiciliarioExistente);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+                throw new Exception(ex.Message.ToString());
+            }
+        }
     }
 }
