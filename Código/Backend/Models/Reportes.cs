ea9769543ffde17bf0c6765641a856b6920a6377
Hector Velasquez <hectordv28@gmail.com>
2025-11-29 19:27:25 -0500

    Pequeños cambios en "ticket".

---
+++ Código/Backend/Models/Reportes.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace SkyHelp.Models
+{
+    public class Reportes
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid IDReporte { get; set; } = Guid.NewGuid();
+        [Required]
+        [StringLength(50)]
+        public string Titulo { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string TipoReporte { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string FechaGeneracion { get; set; }
+        [Required]
+        [ForeignKey("Usuario")]
+        public Guid IDUsuario { get; set; }
+        [Required]
+        public Guid IdOrigen { get; set; } = Guid.NewGuid();
+        [Required]  
+        public String OrigenTabla { get; set; }
+        [JsonIgnore]
+        public virtual Usuarios? Usuario { get; set; }
+
+
+    }
+}
--- Código/Backend/Models/Tickets.cs
+++ Código/Backend/Models/Tickets.cs
@@ -1,15 +1,39 @@
-﻿namespace Microservicios
-{
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace SkyHelp.Models
+{ 
     public class Tickets
     {
-        public Guid IDTicket {  get ; set; }    
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid IDTicket {  get ; set; } = Guid.NewGuid();
+        [Required]
+        [StringLength(200)]
         public string Descripcion { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Categoria { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Prioridad { get; set; }
+        [Required]
         public DateTime FechaCreacion { get; set; } 
+        [Required]
         public Guid IDEstado { get; set; }
-        public Guid IDUsuarioSolicitante { get; set; }
+        [Required]
+        [ForeignKey("Usuario")]
+        public Guid IDUsuario { get; set; }
+        [Required]
+        [ForeignKey("Domiciliario")]
         public Guid IDDomiciliarioAsignado { get; set; }
+        [Required]
+        [ForeignKey("Tecnico")]
         public Guid IDTecnicoAsignado { get; set; }
+        [JsonIgnore]
+        public virtual Usuarios? Usuario { get; set; }
+        [JsonIgnore]
+        public virtual Domiciliarios? Domiciliario { get; set; }
     }
 }
